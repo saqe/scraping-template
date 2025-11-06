b8503bb928fefd4998b29d1377afa8caf6fe25a4
saqib <18554574+saqe@users.noreply.github.com>
2025-11-06 18:42:43 +0500

    FEAT - Chrome driver Constants and ENVs name added.

--- constants.py
+++ constants.py
@@ -1,1 +1,6 @@
+from os import getenv
+
 CONSTANT_ID = ""
+
+CHROME_BROWSER_PATH = getenv("CHROME_BROWSER_PATH", "/opt/chrome/chrome")
+CHROME_DRIVER_PATH = getenv("CHROME_DRIVER_PATH", "/opt/chromedriver")
