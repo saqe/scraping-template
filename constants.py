534d9a463dbce232f05cc3c15e245ed3d87ea066
saqib <18554574+saqe@users.noreply.github.com>
2025-11-06 19:08:06 +0500

    FEAT - Framework sample added.

--- constants.py
+++ constants.py
@@ -1,6 +1,24 @@
+from enum import StrEnum
 from os import getenv
 
 CONSTANT_ID = ""
+
+
+class SampleLocaleEnum(StrEnum):
+    DE = "de-DE"  # Germany
+    EN = "en-GB"  # English
+    FR = "fr-FR"  # France
+    ES = "es-ES"  # Spain
+    IT = "it-IT"  # Italy
+    PL = "pl-PL"  # Poland
+    RO = "ro-RO"  # Romania
+
+
+REQUEST_HEADERS = {
+    "accept": "application/json",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36",
+    "content-type": "application/json",
+}
 
 REDIS_URI = getenv("REDIS_URI")
 
--- models.py
+++ models.py
@@ -2,14 +2,24 @@ from functools import cached_property
 from typing import Optional, Dict, List, Literal, Union
 
 from pydantic import (
-    BaseModel, PositiveInt, HttpUrl, field_validator,
-    model_validator, computed_field, Field, AliasPath, NonNegativeInt
+    BaseModel,
+    PositiveInt,
+    HttpUrl,
+    field_validator,
+    model_validator,
+    computed_field,
+    Field,
+    AliasPath,
+    NonNegativeInt,
+    UUID1,
+    UUID4,
 )
 
 from constants import CONSTANT_ID
 
 
 class SampleModel(BaseModel):
+    id: Union[UUID1, UUID4]
     positive_id: PositiveInt = Field(alias="positiveID")
     price: NonNegativeInt = Field(alias="price")
 
@@ -20,10 +30,11 @@ class SampleModel(BaseModel):
     colors: List[str]
     records: List[Dict]
 
-    this_or_that: Union[int , float, None]
+    this_or_that: Union[int, float, None]
 
     options_type: Literal["option_one", "option_two"] = Field(
-        validation_alias=AliasPath("grand_parent", "child"), default=None)
+        validation_alias=AliasPath("grand_parent", "child"), default=None
+    )
 
     @computed_field
     @cached_property
@@ -37,15 +48,15 @@ class SampleModel(BaseModel):
             return tuple(query_selector[0])
         return None
 
-    @field_validator('positive_id', mode='before')
+    @field_validator("positive_id", mode="before")
     @classmethod
     def validate_seller_id(cls, v: str) -> Optional[PositiveInt]:
         """Clear out M from the Positive id and convert the value to int"""
-        return v and int(v.lstrip('M')) or None
+        return v and int(v.lstrip("M")) or None
 
     @model_validator(mode="after")
     def name_must_contain_space(self):
-        if self.positive_id == 1 and  self.price == 0:
-            raise ValueError('Error Message goes here.')
+        if self.positive_id == 1 and self.price == 0:
+            raise ValueError("Error Message goes here.")
 
         return self
