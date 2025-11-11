3f96990338b331c2770ca3e7cb6d18e83f50d344
saqib <18554574+saqe@users.noreply.github.com>
2025-11-11 16:56:20 +0500

    FEAT - RequestAPI converted into abstract class.

--- utils/http_requests.py
+++ utils/http_requests.py
@@ -1,9 +1,14 @@
+import abc
+
 import requests
 from constants import REQUEST_HEADERS, HTTP_PROXY
 
 
-class RequestAPI:
-    MAIN_URL = ""
+class RequestAPI(abc.ABC):
+    @property
+    @abc.abstractmethod
+    def MAIN_URL(self):
+        raise NotImplementedError
 
     def enable_proxy(self):
         self.session.proxies.update({"http": HTTP_PROXY, "https": HTTP_PROXY})
@@ -11,3 +16,5 @@ class RequestAPI:
     def __init__(self):
         self.session = requests.Session()
         self.session.headers.update(REQUEST_HEADERS)
+        if HTTP_PROXY:
+            self.enable_proxy()
